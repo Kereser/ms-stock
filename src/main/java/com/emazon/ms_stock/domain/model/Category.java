7c17888f7114498e7e8100df1846e9473feb0a6a
Juan Sebastian Urrego <urregoarr00@gmail.com>
2024-08-16 23:05:09 -0500

    Update Category model

--- src/main/java/com/emazon/ms_stock/domain/model/Category.java
+++ src/main/java/com/emazon/ms_stock/domain/model/Category.java
@@ -1,13 +1,35 @@
 package com.emazon.ms_stock.domain.model;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class Category {
-
     private Long id;
     private String name;
     private String description;
+
+    public Category() {
+    }
+
+    public Category(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
