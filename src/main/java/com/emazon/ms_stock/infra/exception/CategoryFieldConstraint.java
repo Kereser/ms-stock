51fe2a298c0489362cedd52d3869801017519660
Juan Sebastian Urrego <urregoarr00@gmail.com>
2024-08-20 18:30:55 -0500

    Delete custom Exception for field validations

--- src/main/java/com/emazon/ms_stock/infra/exception/CategoryFieldConstraint.java
+++
@@ -1,7 +0,0 @@
-package com.emazon.ms_stock.infra.exception;
-
-public class CategoryFieldConstraint extends BaseEntityException {
-    public CategoryFieldConstraint(String field, String reason) {
-        super("Category", field, reason);
-    }
-}
