856e14c347ca8398785b1765ec093035b706d295
Juan Sebastian Urrego <urregoarr00@gmail.com>
2024-08-20 20:47:17 -0500

    Remove data cleaned validations from use case

--- src/main/java/com/emazon/ms_stock/domain/use_cases/CategoryUseCase.java
+++ src/main/java/com/emazon/ms_stock/domain/use_cases/CategoryUseCase.java
@@ -3,20 +3,13 @@ package com.emazon.ms_stock.domain.use_cases;
 
 import com.emazon.ms_stock.domain.api.ICategoryServicePort;
 import com.emazon.ms_stock.domain.model.Category;
-import com.emazon.ms_stock.domain.model.validations.Validations;
 import com.emazon.ms_stock.domain.spi.ICategoryPersistencePort;
 import com.emazon.ms_stock.infra.exception.CategoryAlreadyExists;
-import com.emazon.ms_stock.infra.exception.CategoryFieldConstraint;
-import org.springframework.data.util.Pair;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class CategoryUseCase implements ICategoryServicePort {
 
-    private static final String NAME = "name";
-    private static final String DESCRIPTION = "description";
     private final ICategoryPersistencePort persistencePort;
 
     public CategoryUseCase(ICategoryPersistencePort persistencePort) {
