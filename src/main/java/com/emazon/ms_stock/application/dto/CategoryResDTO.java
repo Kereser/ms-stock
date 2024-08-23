67725bcb710a35ca770698645d42c326d763242e
Juan Sebastian Urrego <urregoarr00@gmail.com>
2024-08-22 20:10:01 -0500

    Add Category response dto

---
+++ src/main/java/com/emazon/ms_stock/application/dto/CategoryResDTO.java
@@ -0,0 +1,12 @@
+package com.emazon.ms_stock.application.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CategoryResDTO {
+    private Long id;
+    private String name;
+    private String description;
+}
