8b74d2990ae63f224154dc7848c533b317a28c2c
Juan Sebastian Urrego <urregoarr00@gmail.com>
2024-08-22 20:09:28 -0500

    Update List -> Page to get all paginated

--- src/main/java/com/emazon/ms_stock/application/handler/IStockHandler.java
+++ src/main/java/com/emazon/ms_stock/application/handler/IStockHandler.java
@@ -1,7 +1,10 @@
 package com.emazon.ms_stock.application.handler;
 
 import com.emazon.ms_stock.application.dto.CategoryReqDTO;
+import com.emazon.ms_stock.application.dto.CategoryResDTO;
+import org.springframework.data.domain.Page;
 
 public interface IStockHandler {
     void saveCategoryInStock(CategoryReqDTO reqDTO);
+    Page<CategoryResDTO> getAllCategories(String order, Integer pageSize, Integer page);
 }
--- src/main/java/com/emazon/ms_stock/domain/spi/ICategoryPersistencePort.java
+++ src/main/java/com/emazon/ms_stock/domain/spi/ICategoryPersistencePort.java
@@ -1,15 +1,16 @@
 package com.emazon.ms_stock.domain.spi;
 
 import com.emazon.ms_stock.domain.model.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface ICategoryPersistencePort {
     void save(Category category);
     void update(Category category);
     void delete(Long id);
-    List<Category> getAll();
-    Category get(Long id);
-    Optional<Category> getByName(String name);
+    Page<Category> findAll(Pageable pageable);
+    Category findById(Long id);
+    Optional<Category> findByName(String name);
 }
