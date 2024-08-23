69f3b2d1551bc32afeab89eb67ff43ebe04caf45
Juan Sebastian Urrego <urregoarr00@gmail.com>
2024-08-22 20:06:08 -0500

    Add mapper

---
+++ src/main/java/com/emazon/ms_stock/infra/out/jpa/mapper/CategoryEntityMapper.java
@@ -0,0 +1,34 @@
+package com.emazon.ms_stock.infra.out.jpa.mapper;
+
+import com.emazon.ms_stock.application.dto.CategoryResDTO;
+import com.emazon.ms_stock.domain.model.Category;
+import com.emazon.ms_stock.infra.out.jpa.entity.CategoryEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        unmappedSourcePolicy = ReportingPolicy.IGNORE)
+public interface CategoryEntityMapper {
+    CategoryEntity toEntity(Category category);
+
+    Category toCategory(CategoryEntity categoryEntity);
+
+    List<Category> toCategoryList(List<CategoryEntity> categoryEntityList);
+
+    List<CategoryResDTO> toCategoryResDTOList(List<Category> categoryList);
+
+    default Page<Category> toCategoryPage(Page<CategoryEntity> categoryEntityList) {
+        List<Category> categoryList = toCategoryList(categoryEntityList.getContent());
+        return new PageImpl<>(categoryList, categoryEntityList.getPageable(), categoryEntityList.getTotalElements());
+    }
+
+    default Page<CategoryResDTO> toCategoryResDTOPage(Page<Category> categoryList) {
+        List<CategoryResDTO> dtoList = toCategoryResDTOList(categoryList.getContent());
+        return new PageImpl<>(dtoList, categoryList.getPageable(), categoryList.getTotalElements());
+    }
+}
