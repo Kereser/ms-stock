94d96dfe4b8fe1effbac78a179d5dbe7f77ec378
Juan Sebastian Urrego <urregoarr00@gmail.com>
2024-08-25 16:18:18 -0500

    Update controller, handler and advisor to control new brand cases

--- src/main/java/com/emazon/ms_stock/application/handler/IStockHandler.java
+++ src/main/java/com/emazon/ms_stock/application/handler/IStockHandler.java
@@ -1,5 +1,6 @@
 package com.emazon.ms_stock.application.handler;
 
+import com.emazon.ms_stock.application.dto.BrandReqDTO;
 import com.emazon.ms_stock.application.dto.CategoryReqDTO;
 import com.emazon.ms_stock.application.dto.CategoryResDTO;
 import com.emazon.ms_stock.application.dto.PageDTO;
@@ -7,4 +8,6 @@ import com.emazon.ms_stock.application.dto.PageDTO;
 public interface IStockHandler {
     void saveCategoryInStock(CategoryReqDTO reqDTO);
     PageDTO<CategoryResDTO> getAllCategories(String order, Integer pageSize, Integer page);
+
+    void createBrandInStock(BrandReqDTO dto);
 }
--- src/main/java/com/emazon/ms_stock/infra/input/rest/StockController.java
+++ src/main/java/com/emazon/ms_stock/infra/input/rest/StockController.java
@@ -1,5 +1,6 @@
 package com.emazon.ms_stock.infra.input.rest;
 
+import com.emazon.ms_stock.application.dto.BrandReqDTO;
 import com.emazon.ms_stock.application.dto.CategoryReqDTO;
 import com.emazon.ms_stock.application.dto.CategoryResDTO;
 import com.emazon.ms_stock.application.dto.PageDTO;
@@ -11,23 +12,29 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/stock/categories")
+@RequestMapping("/stock")
 @RequiredArgsConstructor
 public class StockController {
 
     private final IStockHandler stockHandler;
 
-    @PostMapping
+    @PostMapping("/categories")
     public ResponseEntity<Void> createCategory(@RequestBody @Valid CategoryReqDTO reqDTO) {
         stockHandler.saveCategoryInStock(reqDTO);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @GetMapping
+    @GetMapping("/categories")
     public ResponseEntity<PageDTO<CategoryResDTO>> getAllCategories(
             @RequestParam(defaultValue = "desc") String direction,
             @RequestParam(defaultValue = "20") Integer pageSize,
             @RequestParam(defaultValue = "0") Integer page) {
         return ResponseEntity.ok().body(stockHandler.getAllCategories(direction, pageSize, page));
+    }
+
+    @PostMapping("/brands")
+    public ResponseEntity<Void> createBrand(@RequestBody @Valid BrandReqDTO dto) {
+        stockHandler.createBrandInStock(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
