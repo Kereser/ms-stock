cdb40914c4c760240732b4d1a0ddfc3921c0626a
Juan Sebastian Urrego <urregoarr00@gmail.com>
2024-08-25 16:18:37 -0500

    Update tests

--- src/test/java/com/emazon/ms_stock/domain/use_cases/CategoryUseCaseTest.java
+++ src/test/java/com/emazon/ms_stock/domain/use_cases/CategoryUseCaseTest.java
@@ -3,7 +3,6 @@ package com.emazon.ms_stock.domain.use_cases;
 import com.emazon.ms_stock.domain.model.Category;
 import com.emazon.ms_stock.domain.spi.ICategoryPersistencePort;
 import com.emazon.ms_stock.infra.exception.CategoryAlreadyExists;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -26,16 +25,11 @@ class CategoryUseCaseTest {
     @InjectMocks
     private CategoryUseCase categoryUseCase;
 
-    private Category category;
-
-    @BeforeEach
-    void init() {
-        category = new Category("Test cat", "Test descc");
-    }
+    private final Category category = new Category("Test cat", "Test desc");
 
     @Test
     @DisplayName(value = "Throws an exception when a category with the same name is already saved.")
-    void testExceptionWhenCategoryAlreadyExists() {
+    void Should_throwsAnException_When_CategoryAlreadyExists() {
         Mockito.when(iCategoryPersistencePort.findByName(Mockito.anyString())).thenReturn(Optional.of(category));
 
         assertThrows(CategoryAlreadyExists.class, () -> categoryUseCase.save(category));
@@ -43,7 +37,7 @@ class CategoryUseCaseTest {
 
     @Test
     @DisplayName(value = "Test valid insertion of category.")
-    void testValidSave() {
+    void Should_SaveCategory_When_UniqueName() {
         Mockito.when(iCategoryPersistencePort.findByName(Mockito.anyString())).thenReturn(Optional.empty());
 
         Category cat = new Category("Not same name", "daffsaf");
@@ -54,9 +48,9 @@ class CategoryUseCaseTest {
 
     @Test
     @DisplayName(value = "Test valid calls to persistence port when getting all categories.")
-    void testGetAllSavedCategories() {
-        categoryUseCase.findAll(PageRequest.of(0, 20));
+    void Should_GetAllCategoriesPageable_When_ValidPageRequest() {
+        categoryUseCase.findAllPageable(PageRequest.of(0, 20));
 
-        Mockito.verify(iCategoryPersistencePort, Mockito.times(1)).findAll(Mockito.any());
+        Mockito.verify(iCategoryPersistencePort, Mockito.times(1)).findAllPageable(Mockito.any());
     }
 }
--- src/test/java/com/emazon/ms_stock/infra/input/rest/StockControllerTest.java
+++ src/test/java/com/emazon/ms_stock/infra/input/rest/StockControllerTest.java
@@ -1,5 +1,6 @@
 package com.emazon.ms_stock.infra.input.rest;
 
+import com.emazon.ms_stock.application.dto.BrandReqDTO;
 import com.emazon.ms_stock.application.dto.CategoryReqDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
@@ -24,37 +25,41 @@ class StockControllerTest {
     private MockMvc mockMvc;
     @Autowired
     private ObjectMapper mapper;
-    private String categoryString;
-    private static final String BASIC_URL = "/stock/categories";
+    private String categoryJSON;
+    private static final String BASIC_CATEGORIES_URL = "/stock/categories";
+    private static final String BASIC_BRAND_URL = "/stock/brands";
     private final String validCategoryName = "Test name";
     private final String validCategoryDescription = "description";
+    private String brandJSON;
 
+    /*
+        Category
+     */
     @Test
-    void testCorrectCreationOfCategory() throws Exception {
-        categoryString = mapper.writeValueAsString(CategoryReqDTO.builder().name(validCategoryName).description(validCategoryDescription).build());
-        ResultActions res = sentPostToCreateCategory(categoryString);
+    void Should_CreateCategoryAndGetCorrectStatus_WhenValidPayload() throws Exception {
+        categoryJSON = mapper.writeValueAsString(CategoryReqDTO.builder().name(validCategoryName).description(validCategoryDescription).build());
+        ResultActions res = sentPostToCreateEntity(categoryJSON, BASIC_CATEGORIES_URL);
 
         res.andExpect(status().isCreated());
     }
 
     @Test
-    void testAlreadyExistedCategory() throws Exception {
-        categoryString = mapper.writeValueAsString(CategoryReqDTO.builder().name(validCategoryName).description(validCategoryDescription).build());
-        sentPostToCreateCategory(categoryString);
+    void Should_ThrowsAndException_When_CategoryAlreadyExists() throws Exception {
+        categoryJSON = mapper.writeValueAsString(CategoryReqDTO.builder().name(validCategoryName).description(validCategoryDescription).build());
+        sentPostToCreateEntity(categoryJSON, BASIC_CATEGORIES_URL);
 
-        ResultActions res = sentPostToCreateCategory(categoryString);
-
-        res.andExpect(status().isConflict())
+        sentPostToCreateEntity(categoryJSON, BASIC_CATEGORIES_URL)
+                .andExpect(status().isConflict())
                 .andExpect(jsonPath("$.message").value("Category already exists"))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 
     @Test
-    void testNotValidNullFields() throws Exception {
-        categoryString = mapper.writeValueAsString(CategoryReqDTO.builder().name(null).description(null).build());
-        ResultActions res = sentPostToCreateCategory(categoryString);
+    void Should_GetFieldErrors_When_NullFieldsInPayload() throws Exception {
+        categoryJSON = mapper.writeValueAsString(CategoryReqDTO.builder().name(null).description(null).build());
 
-        res.andExpect(jsonPath("$.message").value("Request has field validation errors"))
+        sentPostToCreateEntity(categoryJSON, BASIC_CATEGORIES_URL)
+                .andExpect(jsonPath("$.message").value("Request has field validation errors"))
                 .andExpect(jsonPath("$.fieldErrors").isMap())
                 .andExpect(jsonPath("$.fieldErrors", Matchers.aMapWithSize(2)))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
@@ -62,11 +67,11 @@ class StockControllerTest {
     }
 
     @Test
-    void testNotValidBlankFields() throws Exception {
-        categoryString = mapper.writeValueAsString(CategoryReqDTO.builder().name("   ").description("   ").build());
-        ResultActions res = sentPostToCreateCategory(categoryString);
+    void Should_GetFieldErrors_When_BlankFieldsInPayload() throws Exception {
+        categoryJSON = mapper.writeValueAsString(CategoryReqDTO.builder().name("   ").description("   ").build());
 
-        res.andExpect(jsonPath("$.message").value("Request has field validation errors"))
+        sentPostToCreateEntity(categoryJSON, BASIC_CATEGORIES_URL)
+                .andExpect(jsonPath("$.message").value("Request has field validation errors"))
                 .andExpect(jsonPath("$.fieldErrors.description").value("must not be blank"))
                 .andExpect(jsonPath("$.fieldErrors.name").value("must not be blank"))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
@@ -74,14 +79,14 @@ class StockControllerTest {
     }
 
     @Test
-    void testNotValidLengthFields() throws Exception {
-        categoryString = mapper.writeValueAsString(CategoryReqDTO.builder()
+    void Should_GetFieldErrors_When_InvalidLengthFieldsInPayload() throws Exception {
+        categoryJSON = mapper.writeValueAsString(CategoryReqDTO.builder()
                 .name("l".repeat(51))
                 .description("k".repeat(91))
                 .build());
-        ResultActions res = sentPostToCreateCategory(categoryString);
 
-        res.andExpect(jsonPath("$.message").value("Request has field validation errors"))
+        sentPostToCreateEntity(categoryJSON, BASIC_CATEGORIES_URL)
+                .andExpect(jsonPath("$.message").value("Request has field validation errors"))
                 .andExpect(jsonPath("$.fieldErrors.description").value("size must be between 3 and 90"))
                 .andExpect(jsonPath("$.fieldErrors.name").value("size must be between 3 and 50"))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
@@ -89,21 +94,42 @@ class StockControllerTest {
     }
 
     @Test
-    void testPageStructureOfAllCategoriesResponse() throws Exception {
-        categoryString = mapper.writeValueAsString(CategoryReqDTO.builder().name(validCategoryName).description(validCategoryDescription).build());
-        sentPostToCreateCategory(categoryString);
+    void Should_GetCorrectObjectFromPageResponse_When_GetAllCategoriesPageable() throws Exception {
+        categoryJSON = mapper.writeValueAsString(CategoryReqDTO.builder().name(validCategoryName).description(validCategoryDescription).build());
+        sentPostToCreateEntity(categoryJSON, BASIC_CATEGORIES_URL);
 
-        ResultActions res = mockMvc.perform(MockMvcRequestBuilders.get(BASIC_URL));
-
-        res.andExpect(jsonPath("$", Matchers.aMapWithSize(4)))
+        mockMvc.perform(MockMvcRequestBuilders.get(BASIC_CATEGORIES_URL))
+                .andExpect(jsonPath("$", Matchers.aMapWithSize(4)))
                 .andExpect(jsonPath("$.totalElements").value(1))
                 .andExpect(jsonPath("$.totalPages").value(1))
                 .andExpect(jsonPath("$.pageable.pageSize").value(20));
     }
 
-    private ResultActions sentPostToCreateCategory(String dto) throws Exception {
-        return mockMvc.perform(MockMvcRequestBuilders.post(BASIC_URL)
+    private ResultActions sentPostToCreateEntity(String dto, String url) throws Exception {
+        return mockMvc.perform(MockMvcRequestBuilders.post(url)
                 .content(dto)
                 .contentType(MediaType.APPLICATION_JSON));
+    }
+
+    /*
+        Brand
+     */
+    @Test
+    void Should_SaveBrand_When_UniqueName() throws Exception {
+        brandJSON = mapper.writeValueAsString(BrandReqDTO.builder().name("Test name").description("Desc").build());
+
+        sentPostToCreateEntity(brandJSON, BASIC_BRAND_URL).andExpect(status().isCreated());
+    }
+
+    @Test
+    void Should_ThrowsAnException_When_BrandAlreadyExists() throws Exception {
+        brandJSON = mapper.writeValueAsString(BrandReqDTO.builder().name("Test name").description("Desc").build());
+
+        sentPostToCreateEntity(brandJSON, BASIC_BRAND_URL).andExpect(status().isCreated());
+
+        sentPostToCreateEntity(brandJSON, BASIC_BRAND_URL)
+                .andExpect(status().isConflict())
+                .andExpect(jsonPath("$.message").value("Brand already exists"))
+                .andExpect(jsonPath("$.fieldErrors.name").value("must be unique"));
     }
 }
