25574da9ba01feb92d7438ac3da685a15e581e4b
Juan Sebastian Urrego <urregoarr00@gmail.com>
2024-08-31 16:10:23 -0500

    Add constant file

---
+++ src/test/java/com/emazon/ms_stock/ConsUtils.java
@@ -0,0 +1,44 @@
+package com.emazon.ms_stock;
+
+public class ConsUtils {
+    public static final String BASIC_CATEGORIES_URL = "/stock/categories";
+    public static final String BASIC_BRAND_URL = "/stock/brands";
+    public static final String BASIC_ARTICLES_URL = "/stock/articles";
+
+    public static final String TEST_NAME = "Test name";
+    public static final String DESC_NAME = "description";
+    public static final String FIELD_ERROR = "$.fieldErrors";
+    public static final String FIELD_MESSAGE = "$.message";
+    public static final String FIELD_NAME_PATH = "$.fieldErrors.name";
+    public static final String FIELD_DESCRIPTION_PATH = "$.fieldErrors.description";
+    public static final String FIELD_CATEGORYIDS_PATH = "$.fieldErrors.categoryIds";
+    public static final String FIELD_PRICE_PATH = "$.fieldErrors.price";
+    public static final String FIELD_QUANTITY_PATH = "$.fieldErrors.quantity";
+    public static final String FIELD_BRANDID_PATH = "$.fieldErrors.brandId";
+    public static final String FIELD_DIRECTION_PATH = "$.fieldErrors.direction";
+
+    public static final Integer FIELD_WITH_ERRORS_AT_CATEGORY = 2;
+    public static final Integer FIELD_WITH_ERRORS_AT_BRAND = 2;
+    public static final Integer FIELD_WITH_ERRORS_AT_ARTICLE = 6;
+
+    public static final Long LONG_ONE = 1L;
+
+    public static final Integer INT_ZERO = 0;
+
+    public static final String PLUS_FIFTY_CHARACTERS = "d".repeat(51);
+    public static final String PLUS_NINETY_CHARACTERS = "d".repeat(91);
+    public static final String PLUS_TWO_HUNDRED_CHARACTERS = "d".repeat(201);
+
+    public static final String SORT_BY_NAME = "name";
+    public static final Integer STARTER_PAGE = 0;
+    public static final Integer DEFAULT_PAGE_SIZE = 20;
+
+    public static final String INVALID_SORT_CRITERIA = "Invalid sort criteria in request param";
+    public static final String NON_EXISTING_SORTING_COLUMN = "Non:existing:column";
+    public static final String COLUMN_PARAM = "column";
+
+    public static final String NON_EXISTING_DIRECTION_COLUMN = "Non:existing:DIRECTION";
+    public static final String DIRECTION_PARAM = "direction";
+
+    public static final String ERROR_PROCESSING_OPERATION = "Error processing operation with: ";
+}
