a44a7662bfaac15d790acf531daa2bdb2a3ecf03
Juan Sebastian Urrego <urregoarr00@gmail.com>
2024-09-13 18:41:46 -0500

    Add new methods to interfaces and delete not used ones

--- src/main/java/com/emazon/ms_stock/domain/api/IArticleServicePort.java
+++ src/main/java/com/emazon/ms_stock/domain/api/IArticleServicePort.java
@@ -1,6 +1,10 @@
 package com.emazon.ms_stock.domain.api;
 
+import com.emazon.ms_stock.application.dto.supply.SupplyReqDTO;
 import com.emazon.ms_stock.domain.model.Article;
 
+import java.util.Set;
+
 public interface IArticleServicePort extends IBasicCrudServicePort<Article> {
+    void addSupply(Set<SupplyReqDTO> dto);
 }
--- src/main/java/com/emazon/ms_stock/domain/spi/IArticlePersistencePort.java
+++ src/main/java/com/emazon/ms_stock/domain/spi/IArticlePersistencePort.java
@@ -4,10 +4,13 @@ import com.emazon.ms_stock.application.dto.PageDTO;
 import com.emazon.ms_stock.application.dto.PageHandler;
 import com.emazon.ms_stock.domain.model.Article;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface IArticlePersistencePort extends IBasicCrudPersistencePort<Article> {
     Optional<Article> findByName(String name);
     PageDTO<Article> findAllByCategoryNameAsc(PageHandler page);
     PageDTO<Article> findAllByCategoryNameDesc(PageHandler page);
+    List<Article> findAllById(Iterable<Long> ids);
+    void saveAll(Iterable<Article> articles);
 }
