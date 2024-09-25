4631d32bd5149756ebb2491ee438ec761c0761c1
Juan Sebastian Urrego <urregoarr00@gmail.com>
2024-09-24 20:35:12 -0500

    Get cart articles

--- src/main/java/com/emazon/ms_stock/infra/out/jpa/adapter/ArticleJpaAdapter.java
+++ src/main/java/com/emazon/ms_stock/infra/out/jpa/adapter/ArticleJpaAdapter.java
@@ -1,17 +1,17 @@
 package com.emazon.ms_stock.infra.out.jpa.adapter;
 
-import com.emazon.ms_stock.application.dto.PageDTO;
-import com.emazon.ms_stock.application.dto.PageHandler;
-import com.emazon.ms_stock.application.utils.ParsingUtils;
+import com.emazon.ms_stock.application.dto.handlers.PageDTO;
+import com.emazon.ms_stock.application.dto.out.ArticlesPriceDTO;
 import com.emazon.ms_stock.domain.model.Article;
 import com.emazon.ms_stock.domain.spi.IArticlePersistencePort;
 import com.emazon.ms_stock.infra.out.jpa.entity.ArticleEntity;
 import com.emazon.ms_stock.infra.out.jpa.mapper.ArticleEntityMapper;
 import com.emazon.ms_stock.infra.out.jpa.repository.ArticleJpaRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.Set;
 
 @RequiredArgsConstructor
 public class ArticleJpaAdapter implements IArticlePersistencePort {
@@ -25,24 +25,24 @@ public class ArticleJpaAdapter implements IArticlePersistencePort {
     }
 
     @Override
-    public PageDTO<Article> findAllPageable(PageHandler page) {
-        PageDTO<ArticleEntity> pageEntity = mapper.toArticleEntityPage(repository.findAll(ParsingUtils.toPageable(page)));
+    public PageDTO<Article> findAllByArticleIdPageable(Pageable page, Set<Long> articleIds) {
+        PageDTO<ArticleEntity> pageEntity = mapper.articleEntityPageToArticleEntityPageDTO(repository.findAllByArticleIdPageable(page, articleIds));
 
-        return mapper.toArticlePage(pageEntity);
+        return mapper.articleEntityPageDTOToArticlePageDTO(pageEntity);
     }
 
     @Override
-    public PageDTO<Article> findAllByCategoryNameAsc(PageHandler page) {
-        PageDTO<ArticleEntity> pageEntity = mapper.toArticleEntityPage(repository.findAllArticlesOrderByCategoryNameASC(ParsingUtils.toPageableUnsorted(page)));
+    public PageDTO<Article> findAllByCategoryNameAsc(Pageable page, Set<Long> articleIds) {
+        PageDTO<ArticleEntity> pageEntity = mapper.articleEntityPageToArticleEntityPageDTO(repository.findAllArticlesOrderByCategoryNameASC(page, articleIds));
 
-        return mapper.toArticlePage(pageEntity);
+        return mapper.articleEntityPageDTOToArticlePageDTO(pageEntity);
     }
 
     @Override
-    public PageDTO<Article> findAllByCategoryNameDesc(PageHandler page) {
-        PageDTO<ArticleEntity> pageEntity = mapper.toArticleEntityPage(repository.findAllArticlesOrderByCategoryNameDESC(ParsingUtils.toPageableUnsorted(page)));
+    public PageDTO<Article> findAllByCategoryNameDesc(Pageable page, Set<Long> articleIds) {
+        PageDTO<ArticleEntity> pageEntity = mapper.articleEntityPageToArticleEntityPageDTO(repository.findAllArticlesOrderByCategoryNameDESC(page, articleIds));
 
-        return mapper.toArticlePage(pageEntity);
+        return mapper.articleEntityPageDTOToArticlePageDTO(pageEntity);
     }
 
     @Override
@@ -52,6 +52,23 @@ public class ArticleJpaAdapter implements IArticlePersistencePort {
 
     @Override
     public void saveAll(Iterable<Article> articles) {
-        repository.saveAll(mapper.toListEntity(articles));
+        repository.saveAll(mapper.articlesToArticleEntities(articles));
+    }
+
+    @Override
+    public PageDTO<Article> findAllByArticleIdsAndCategoryAndBrandNameAsc(Pageable page, Set<Long> articleIds) {
+        PageDTO<ArticleEntity> articleEntityPageDTO = mapper.articleEntityPageToArticleEntityPageDTO(repository.findAllByArticleIdsAndCategoryAndBrandNameAsc(page, articleIds));
+        return mapper.articleEntityPageDTOToArticlePageDTO(articleEntityPageDTO);
+    }
+
+    @Override
+    public PageDTO<Article> findAllByArticleIdsCategoryAndBrandNameDesc(Pageable page, Set<Long> articleIds) {
+        PageDTO<ArticleEntity> articleEntityPageDTO = mapper.articleEntityPageToArticleEntityPageDTO(repository.findAllByArticleIdsAndCategoryAndBrandNameDesc(page, articleIds));
+        return mapper.articleEntityPageDTOToArticlePageDTO(articleEntityPageDTO);
+    }
+
+    @Override
+    public Set<ArticlesPriceDTO> getArticlesPrice(Set<Long> articleIds) {
+        return mapper.articleEntitiesToArticlesPriceDTO(repository.findAllById(articleIds));
     }
 }
