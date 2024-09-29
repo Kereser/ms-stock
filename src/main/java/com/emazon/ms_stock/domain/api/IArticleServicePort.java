a6f0dbb310907b7b7f4f49ab302dcfd3f8f54bb9
Juan Sebastian Urrego <urregoarr00@gmail.com>
2024-09-29 16:47:14 -0500

    Update interfaces with new methods to process validations, reduction and rollback of stock

--- src/main/java/com/emazon/ms_stock/domain/api/IArticleServicePort.java
+++ src/main/java/com/emazon/ms_stock/domain/api/IArticleServicePort.java
@@ -1,13 +1,20 @@
 package com.emazon.ms_stock.domain.api;
 
 import com.emazon.ms_stock.application.dto.ItemQuantityDTO;
+import com.emazon.ms_stock.application.dto.ItemsReqDTO;
 import com.emazon.ms_stock.application.dto.out.ArticlesPriceDTO;
 import com.emazon.ms_stock.domain.model.Article;
 
+import java.util.List;
 import java.util.Set;
 
 public interface IArticleServicePort extends IBasicCrudServicePort<Article> {
     void addSupply(Set<ItemQuantityDTO> dto);
-    void handleCartAdditionValidations(Set<ItemQuantityDTO> dto);
+    void validationsOnStockForCart(Set<ItemQuantityDTO> dto);
     Set<ArticlesPriceDTO> getArticlesPrice(Set<Long> articleIds);
+    List<Article> getAllArticles(List<Long> articleIds);
+
+    void processStockReduction(ItemsReqDTO itemsReqDTO);
+
+    void processRollback(ItemsReqDTO itemsReqDTO);
 }
