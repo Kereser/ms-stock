f41d05aa0f4743e742ea53b2473fc9f89f0139c1
Juan Sebastian Urrego <urregoarr00@gmail.com>
2024-10-13 20:33:27 -0500

    Update interfaces and mappers

--- src/main/java/com/emazon/ms_stock/domain/spi/IBrandPersistencePort.java
+++ src/main/java/com/emazon/ms_stock/domain/spi/IBrandPersistencePort.java
@@ -4,10 +4,13 @@ import com.emazon.ms_stock.application.dto.handlers.PageDTO;
 import com.emazon.ms_stock.domain.model.Brand;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface IBrandPersistencePort extends IBasicCrudPersistencePort<Brand> {
     Optional<Brand> findByName(String name);
     Optional<Brand> findById(Long id);
     PageDTO<Brand> findAllPageable(Pageable page);
+
+    List<Brand> findAllByNames(List<String> names);
 }
