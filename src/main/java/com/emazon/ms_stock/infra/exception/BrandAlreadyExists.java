135002ae86c3c3060cda9bce9aef14d2213fe6b4
Juan Sebastian Urrego <urregoarr00@gmail.com>
2024-11-18 14:15:47 -0500

    Constants

--- src/main/java/com/emazon/ms_stock/infra/exception/BrandAlreadyExists.java
+++ src/main/java/com/emazon/ms_stock/infra/exception/BrandAlreadyExists.java
@@ -1,7 +1,9 @@
 package com.emazon.ms_stock.infra.exception;
 
+import com.emazon.ms_stock.ConsUtils;
+
 public class BrandAlreadyExists extends BaseEntityException {
-    public BrandAlreadyExists() {
-        super("Brand", "name");
-    }
+  public BrandAlreadyExists() {
+    super("Brand", ConsUtils.NAME);
+  }
 }
